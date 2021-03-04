398221ca2c676778ef8e0be5bcc7c406802e2e0d
Juan Diego Coto <jd506@hotmail.com>
2021-03-04 03:28:48 +0000

    Connecting fetch and checking empty fields

--- src/js/component/login.js
+++ src/js/component/login.js
@@ -1,70 +1,92 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Form, Col, Row, Button, Card, Accordion, footer } from "react-bootstrap";
+// import { Redirect } from "react-router-dom";
 
 export const Main = () => {
-	// const [email, setEmail] = useState("");
-	// const [pass, setPass] = useState("");
+	const [email, setEmail] = useState("");
+	const [pass, setPass] = useState("");
+	//const [redirect, setRedirect] = useState(false);
 
-	// //**********************************************LOG  IN**********************************************//
-	// const handleSubmit = e => {
-	// 	e.preventDefault();
-	// 	if (email === "" || pass === "") {
-	// 		alert("correo y contraseña son requeridos");
-	// 	}
-	// 	console.log(email, pass);
+	//**********************************************LOG  IN**********************************************//
+	const handleSubmitlogin = e => {
+		e.preventDefault();
+		if (email === "" || pass === "") {
+			alert("Correo y contraseña son requeridos");
+		}
+		console.log(email, pass);
 
-	// 	// FETCH
-	// 	const data = { email: email, password: pass };
+		// FETCH
+		const data = { email: email, password: pass }; // Objeto que me manejara los fields que se van a ingresar en el register y login
 
-	// 	fetch("https://3000-emerald-jellyfish-qyn0ga41.ws-us03.gitpod.io/login", {
-	// 		method: "POST",
-	// 		headers: {
-	// 			"Content-Type": "application/json"
-	// 		},
-	// 		body: JSON.stringify(data)
-	// 	})
-	// 		.then(response => response.json())
-	// 		.then(data => {
-	// 			console.log("Success:", data);
-	// 			sessionStorage.setItem("u_token", data.token);
-	// 			setRedirect(true);
-	// 		})
-	// 		.catch(error => {
-	// 			console.error("Error:", error);
-	// 		});
+		fetch("https://3000-indigo-duck-4s99l8l5.ws-us03.gitpod.io/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json" //Lo que se va a enviar es json, por eso se pone el header
+			},
+			body: JSON.stringify(data) //Convierte todo a un String , se envia como stringfy porque en el back end, se recibe como "email", "password"
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Success:", data); //imprime la data, si se registro o no se registro
+				sessionStorage.setItem("u_token", data.token);
+				//setRedirect(true);
+			})
+			.catch(error => {
+				console.error("Error:", error); //Cualquier error que pueda ocurrir
+			});
 
-	// 	// setRedirect(true);
-	// };
+		// setRedirect(true);
+	};
 	// //**********************************************SIGN iN**********************************************//
-	// const handleSubmit = e => {
-	// 	e.preventDefault();
-	// 	if (email === "" || pass === "") {
-	// 		alert("correo y contraseña son requeridos");
-	// 	}
-	// 	console.log(email, pass);
+	const handleSubmitRegister = e => {
+		e.preventDefault();
+		if (email === "" || pass === "") {
+			alert("Correo y contraseña son requeridos");
+		}
+		console.log(email, pass);
 
-	// 	// FETCH
-	// 	const data = { email: email, password: pass };
+		// FETCH
+		const data = { email: email, password: pass };
 
-	// 	fetch("https://3000-emerald-jellyfish-qyn0ga41.ws-us03.gitpod.io/register", {
-	// 		method: "POST",
-	// 		headers: {
-	// 			"Content-Type": "application/json"
-	// 		},
-	// 		body: JSON.stringify(data)
-	// 	})
-	// 		.then(response => response.json())
-	// 		.then(data => {
-	// 			console.log("Success:", data);
-	// 			setRedirect(true);
-	// 		})
-	// 		.catch(error => {
-	// 			console.error("Error:", error);
-	// 		});
+		fetch("https://3000-indigo-duck-4s99l8l5.ws-us03.gitpod.io/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(data)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Success:", data);
+				//setRedirect(true);
+			})
+			.catch(error => {
+				console.error("Error:", error);
+			});
 
-	// 	// setRedirect(true);
-	// };
+		// setRedirect(true);
+	};
+
+	const testPrivado = () => {
+		fetch("https://3000-indigo-duck-4s99l8l5.ws-us03.gitpod.io/profile", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "Bearer " + sessionStorage.getItem("u_token")
+			}
+			// body: JSON.stringify(data)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Success:", data);
+				// sessionStorage.setItem("u_token", data.token);
+				// setRedirect(true);
+			})
+			.catch(error => {
+				console.error("Error:", error);
+			});
+	};
 
 	return (
 		<div className="container log text-center d-flex justify-content-center " fluid>
@@ -87,27 +109,36 @@ export const Main = () => {
 									<Accordion.Collapse eventKey="0">
 										<Card.Body>
 											<div className="text-center">
-												<Form>
+												<Form onSubmit={e => handleSubmitlogin(e)}>
 													<Form.Row>
 														<Form.Group as={Col} controlId="formGridEmail">
 															<Form.Label>Email</Form.Label>
 															<Form.Control
 																type="email"
 																placeholder="someuser@gmail.com"
+																onChange={e => setEmail(e.target.value)}
 															/>
 														</Form.Group>
 
 														<Form.Group as={Col} controlId="formGridPassword">
 															<Form.Label>Password</Form.Label>
-															<Form.Control type="password" placeholder="Password" />
+															<Form.Control
+																type="password"
+																placeholder="Password"
+																onChange={e => setPass(e.target.value)}
+															/>
 														</Form.Group>
 													</Form.Row>
-													<Link to="/Menu">
-														<Button variant="primary" type="submit">
-															Log in
-														</Button>
-													</Link>
+													{/* <Link to="/Menu"> */}
+													<Button
+														variant="primary"
+														type="submit"
+														onClick={() => testPrivado()}>
+														Log in
+													</Button>
+													{/* </Link> */}
 												</Form>
+												{/* {redirect ? <Redirect to="/login" /> : ""} */}
 											</div>
 										</Card.Body>
 									</Accordion.Collapse>
@@ -121,29 +152,31 @@ export const Main = () => {
 									<Accordion.Collapse eventKey="1">
 										<Card.Body>
 											<div className="text-center">
-												<Form>
-													<Form.Group controlId="formGridAddress1">
-														<Form.Label>Name</Form.Label>
-														<Form.Control placeholder="Write your name" />
-													</Form.Group>
+												<Form onSubmit={e => handleSubmitRegister(e)}>
 													<Form.Row>
 														<Form.Group as={Col} controlId="formGridEmail">
 															<Form.Label>Email</Form.Label>
 															<Form.Control
 																type="email"
 																placeholder="someuser@gmail.com"
+																onChange={e => setEmail(e.target.value)}
 															/>
 														</Form.Group>
 
 														<Form.Group as={Col} controlId="formGridPassword">
 															<Form.Label>Password</Form.Label>
-															<Form.Control type="password" placeholder="Password" />
+															<Form.Control
+																type="password"
+																placeholder="Password"
+																onChange={e => setPass(e.target.value)}
+															/>
 														</Form.Group>
 													</Form.Row>
 													<Button variant="primary" type="submit">
-														Submit
+														Sign in
 													</Button>
 												</Form>
+												{/* {redirect ? <Redirect to="/login" /> : ""} */}
 											</div>
 										</Card.Body>
 									</Accordion.Collapse>
